08904ae78267be01c82434bc12689eaa1d45d7d2
gayathri-ch06 <124899858+gayathri-ch06@users.noreply.github.com>
2023-04-17 19:43:09 -0400

    Adding service requests code
    services.py includes the API requests made by client to server.

---
+++ services/services.py
@@ -0,0 +1,77 @@
+import requests
+
+
+def login(email, password, userType):
+    url = f"http://localhost:8000/login/{userType}"
+
+    payload = {
+        "email": email,
+        "password": password
+    }
+
+    response = requests.request("POST", url, json=payload)
+    return response.json()['status']
+
+
+def enrollCourse(courseName):
+    url = f"http://localhost:8000/enrollCourse/{courseName}"
+
+    response = requests.request("POST", url)
+    return response.json()
+
+
+def submit(data, pageName):
+    url = f"http://localhost:8000/submit/{pageName}"
+
+    response = requests.request("POST", url, json=data)
+    return response.json()
+
+
+def getStudents():
+    url = f"http://localhost:8000/getData/students"
+    response = requests.request("GET", url)
+    return response.json()
+
+
+def getAssignments():
+    url = f"http://localhost:8000/getData/assignment"
+    response = requests.request("GET", url)
+    return response.json()
+
+
+def getQuizzes():
+    url = f"http://localhost:8000/getData/quiz"
+    response = requests.request("GET", url)
+    return response.json()
+
+
+def getFeedback():
+    url = f"http://localhost:8000/getData/feedback"
+    response = requests.request("GET", url)
+    return response.json()
+
+
+def uploadFile(name, path, ext, userType, fileType):
+    url = f"http://localhost:8000/upload/{userType}?filetype={fileType}"
+
+    payload = {}
+    files = [
+        ('file', (name, open(path, 'rb'), f'application/{ext}'))
+    ]
+    headers = {}
+
+    response = requests.request("POST", url, headers=headers, data=payload, files=files)
+
+    return response.json()
+
+
+def download_file(file_path, path):
+    # Display a file dialog box to allow the user to select the download location
+
+    # Download the file from the URL
+    url = f'http://localhost:8000/{path}'
+    response = requests.get(url)
+
+    # Write the downloaded file to disk
+    with open(file_path, 'wb') as f:
+        f.write(response.content)
